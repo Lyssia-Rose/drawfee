824c24961cd5d27245add9df77ec7a795d0e02d8
Mia <Mia@example.com>
2020-12-14 14:03:43 +0000

    cleaned up links page

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ cat src/footer.html | sed 's/__NUMBER__/3800/' >> gallery.html
 
 cat src/header.html | sed 's/__TITLE__/Drawfee - Links/' > links.html
 cat src/links.html >> links.html
-cat src/footer.html | sed 's/__NUMBER__/1200/' >> links.html
+cat src/footer.html | sed 's/__NUMBER__/1050/' >> links.html
 
